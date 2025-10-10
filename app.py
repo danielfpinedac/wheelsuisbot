e479f4cc58587e200da951339a76c83fcecce5b6
Daniel Felipe Pineda Capacho <danielfpinedac@gmail.com>
2025-10-10 15:54:47 -0500

    Configurar Flask para usar puerto dinámico en Render

--- app.py
+++ app.py
@@ -108,4 +108,6 @@ def join_group(barrio):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
